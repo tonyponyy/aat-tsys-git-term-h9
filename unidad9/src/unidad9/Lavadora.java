34d94acef6f9a32ece3041bbfb4a6b68c47a8c0e
Ainara <ainarabliz@gmail.com>
2023-10-06 18:41:27 +0200

    Lavadora final

--- unidad9/src/unidad9/Lavadora.java
+++ unidad9/src/unidad9/Lavadora.java
@@ -1,5 +1,38 @@
 package unidad9;
 
-public class Lavadora {
+public class Lavadora extends Electrodomestico{
+	
+	private final double CARGA = 5;
+	protected double carga;
+	
+	public Lavadora() {
+		super();
+		this.carga = CARGA;
+	}
+	public Lavadora(double precio, double peso) {
+		super(precio, peso);
+		this.carga = CARGA;
+		
+	}
+	public Lavadora(double precio, String color, char consumo, double peso) {
+		super(precio, peso, color, consumo);
+		this.carga = CARGA;
+	}
+	public double getCarga() {
+		return carga;
+	}
+	
+	public double getCARGA() {
+		return CARGA;
+	}
+	@Override
+	public double precioFinal() {
+		double total = 0;
+		if (carga<30) {
+			total=(total+50);
+		}
+		return super.precioFinal()+total;
+	}
+	
 
 }
